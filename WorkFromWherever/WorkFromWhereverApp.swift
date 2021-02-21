03a029bebf241bacbe0a0d2dc0f9b06c8e5be551
Jay Stakelon <stakes@users.noreply.github.com>
2021-02-21 15:39:33 -0800

    Initial Commit

---
+++ WorkFromWherever/WorkFromWhereverApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkFromWhereverApp.swift
+//  WorkFromWherever
+//
+//  Created by Jay Stakelon on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkFromWhereverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
