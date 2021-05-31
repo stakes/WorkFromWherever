7c6f20fd6f254fbc80a0a6a9a7b9c13d68167d46
Jay Stakelon <stakes@users.noreply.github.com>
2021-05-31 16:22:30 -0700

    button hit areas

--- WorkFromWherever/SelectorView.swift
+++ WorkFromWherever/SelectorView.swift
@@ -16,24 +16,39 @@ struct SelectorView: View {
             HStack {
                 Text(channelListViewModel.channelList.channels[selectedChannelIndex].title).font(.system(.title3, design: .monospaced)).foregroundColor(.white).padding(8).padding(.leading, 12).opacity(0.8)
                 Spacer()
+                
+                
                 Button(action: {
                     if (self.selectedChannelIndex > 0) {
                         self.soundManager.removeAllTracks()
                         self.selectedChannelIndex -= 1
+                    } else {
+                        self.soundManager.removeAllTracks()
+                        self.selectedChannelIndex = channelListViewModel.channelList.channels.count-1
                     }
 
                 }) {
-                    Image(systemName: "chevron.left").foregroundColor(.white)
+                    Image(systemName: "chevron.left")
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .contentShape(Rectangle())
                 }.buttonStyle(PlainButtonStyle())
                 Button(action: {
                     if (self.selectedChannelIndex < channelListViewModel.channelList.channels.count-1) {
                         self.soundManager.removeAllTracks()
                         self.selectedChannelIndex += 1
+                    } else {
+                        self.soundManager.removeAllTracks()
+                        self.selectedChannelIndex = 0
                     }
                 }) {
-                    Image(systemName: "chevron.right").foregroundColor(.white)
+                    Image(systemName: "chevron.right")
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .contentShape(Rectangle())
                 }.buttonStyle(PlainButtonStyle())
                 .padding(.trailing, 8)
+                
             }.background(LinearGradient(gradient: Gradient(colors: [Color("screenColorStart"), Color("screenColorStop")]), startPoint: .bottom, endPoint: .top))
             .cornerRadius(8.0)
             .padding(.top, 32)
